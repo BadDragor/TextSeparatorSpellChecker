cd91172d914747cb2bb0f5fd6d760ad3d629c98a
SweetAI Belle <SweetAIBBelle@gmail.com>
2022-10-29 23:43:45 -0700

    Reformat file.

--- TextSeparatorForm.vb
+++ TextSeparatorForm.vb
@@ -12,8 +12,6 @@
 
     Private objTemp0, objTemp1, objTemp2, objTemp3, objTemp4, objTemp5, objTemp6, objTemp7, objTemp8, objTemp9 As Object
 
-
-
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         '#If DEBUG Then
         '        txtContentPath.Text = "C:\Temp\content.txt"
@@ -21,17 +19,12 @@
 
         '#End If
 
-
         CheckForTheNewestVersionOfTheProgramToolStripMenuItem.Text = CheckForTheNewestVersionOfTheProgramToolStripMenuItem.Text & " (current " & VersionToolStripMenuItem.Text.ToLower & ")"
 
     End Sub
 
-
     Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
         strRootProgramPath = Application.StartupPath
-
-
-        'strRootProgramPath = System.IO.Path.GetPathRoot(strProgramPath)
 
         If System.IO.File.Exists(strRootProgramPath & "\" & "SavedData.txt") Then
             Dim objReader As New System.IO.StreamReader(strRootProgramPath & "\" & "SavedData.txt", System.Text.Encoding.GetEncoding("windows-1250"), True)
@@ -61,13 +54,7 @@
         End If
     End Sub
 
-
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
-
-        'System.IO.File.Delete(strRootProgramPath & "\" & "SavedData.txt")
-
-        'System.Threading.Thread.Sleep(200)
-
         Dim objWriter As New System.IO.StreamWriter(strRootProgramPath & "\" & "SavedData.txt", False)
         'An unhandled exception of type 'System.UnauthorizedAccessException' occurred in mscorlib.dll  Additional information: Access to the path 'C:\SavedData.txt' is denied.
         objWriter.Write(txtContentPath.Text & vbCrLf & txtGrammarPath.Text)
@@ -75,10 +62,7 @@
 
     End Sub
 
-
-
     Private Sub bttSeparate_Click(sender As Object, e As EventArgs) Handles bttSeparate.Click
-
         dgvSeparated.Rows.Clear()
 
         strTekst = ""
@@ -91,31 +75,23 @@
 
             If objLine IsNot Nothing Then
                 strTekst &= objLine & vbCrLf
-
             End If
-
-
         Loop Until objLine Is Nothing
 
         objReader.Close()
 
-
         objTemp0 = strTekst.ToString.Length
-        For i = 0 To objTemp0 - 1
 
+        For i = 0 To objTemp0 - 1
             If strTempText.Length < txtNUMBER_OF_CHARACTERS.Text Then
                 strTempText &= strTekst.ToString.Substring(i, 1)
             Else
-
                 AddRow()
                 i = i - txtNUMBER_OF_REPEAT_CHARACTERS.Text
             End If
-
         Next
+
         AddRow()
-
-
-
     End Sub
 
     Private Sub AddRow()
@@ -136,7 +112,6 @@
     End Sub
 
     Private Sub dgvSeparated_CellEnter(sender As Object, e As DataGridViewCellEventArgs) Handles dgvSeparated.CellEnter
-
         Dim intLine As Integer = 0
         Dim intConsecutiveCommentLine As Integer = 0
 
@@ -148,7 +123,6 @@
                 Clipboard.SetText(objTemp0)
                 'An unhandled exception of type 'System.ArgumentNullException' occurred in System.Windows.Forms.dll Additional information: Value cannot be null.
             Catch ex As Exception
-
             End Try
 
             dgvSeparated.Rows(e.RowIndex).Cells("ENTERED").Value = 1
@@ -168,7 +142,6 @@
                 Exit For
             End If
         Next
-
     End Sub
 
     Private Sub bttOpen_Click(sender As Object, e As EventArgs) Handles bttOpenStory.Click
@@ -185,9 +158,6 @@
             MsgBox(ex.Message)
         End Try
     End Sub
-
-
-
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
@@ -224,17 +194,9 @@
 #End If
         End Try
 
-
-
-
-
-
-
-
         dgvSeparated.Rows.Clear()
 
         strTekst = ""
-
 
         Dim blnSkip As Boolean = False
         Dim strAllCharacters As String = "abcčdefgijklmnoprsštuvzžyxqABCČDEFGIJKLMNOPRSŠTUVZŽYXQ"
@@ -276,10 +238,12 @@
                 lstGrammarFile.Add(objGrammarLine)
             End If
         Loop Until objGrammarLine Is Nothing
+
         objReader2.Close()
 
         'intContentCounter += 1
         Dim objReader As New System.IO.StreamReader(txtContentPath.Text, System.Text.Encoding.GetEncoding("windows-1250"), True)
+
         Do
             'intContentCounter += 1
             Try
@@ -294,22 +258,14 @@
                 lstContentFile.Add(objLine)
             End If
         Loop Until objLine Is Nothing
+
         objReader.Close()
 
-        'Do
-
-        'Try
-        '    objGrammarLine = objReader2.ReadLine()
-        '    'An unhandled exception of type 'System.ObjectDisposedException' occurred in mscorlib.dll Additional information: Cannot read from a closed TextReader.   'Solution: Open it for every line of content.
-        'Catch ex As Exception
-        '    MsgBox(ex.Message & vbCrLf & "intContentCounter = " & intContentCounter & vbCrLf & "intGrammarCounter = " & intGrammarCounter)
-        'End Try
         For j = 0 To lstGrammarFile.Count - 1
-
             blnGrammarTagWasUsed = False
             objGrammarLine = lstGrammarFile(j)
-
             intGrammarCounter += 1
+
             For k = 0 To lstContentFile.Count - 1
                 intContentCounter += 1
                 'Do
@@ -319,7 +275,6 @@
                 Catch ex As Exception
                     MsgBox(ex.Message & vbCrLf & "intContentCounter = " & intContentCounter & vbCrLf & "intGrammarCounter = " & intGrammarCounter)
                 End Try
-
 
                 If objLine IsNot Nothing Then
                     'objGrammarLine = objLine
@@ -383,21 +338,11 @@
         Next
 
         objReader.Close()
-        'System.Threading.Thread.Sleep(200) 'Waits for file to close.
-
-        'Loop Until objGrammarLine Is Nothing
-
-        'objReader2.Close()
-
-
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles bttOpenGrammar.Click
         joined_bttOpen_Click(txtGrammarPath.Text)
-
     End Sub
-
-
 
     Private Sub txtContentPath_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles txtContentPath.MouseDoubleClick
         Dim OpenFileDialog As New OpenFileDialog
@@ -454,9 +399,9 @@
 
     End Sub
 
-
     Private Sub ReportABugImprovementToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReportABugImprovementToolStripMenuItem.Click
         System.Diagnostics.Process.Start("https://github.com/BadDragor/TextSeparatorSpellChecker/issues")
 
     End Sub
+
 End Class
